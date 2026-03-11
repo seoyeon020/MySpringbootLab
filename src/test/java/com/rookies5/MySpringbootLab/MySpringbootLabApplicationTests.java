d3fd8faf7955e618ae927b3e2fb5d0a19c0f6f8a
seoyeon020 <ksheee7000@naver.com>
2026-03-11 22:58:27 +0900

    first commit

---
+++ src/test/java/com/rookies5/MySpringbootLab/MySpringbootLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookies5.MySpringbootLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
